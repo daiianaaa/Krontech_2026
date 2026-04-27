8f8c79e534417c184b5abac54000affeffd5b4a2
daiianaaa <horvathdaiana@yahoo.com>
2026-04-27 16:13:20 +0300

    I made the tasks for the first epic: I initialized the angular app and I listed the medicine on it

--- medistock-frontend/src/app/app.config.ts
+++ medistock-frontend/src/app/app.config.ts
@@ -1,13 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    provideHttpClient(withInterceptors([authInterceptor])),
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
--- medistock-frontend/src/app/app.ts
+++ medistock-frontend/src/app/app.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Medicament } from './models/medicament';
+import { MEDICAMENTS_MOCK } from './data/medicaments.mock';
+import { MedicamentListComponent } from './components/medicament-list/medicament-list';
+import { MedicationForm } from './components/medication-form/medication-form';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
-  styles: []
+  imports: [MedicamentListComponent, MedicationForm],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  medicaments: Medicament[] = MEDICAMENTS_MOCK;
+
+  addMed(m: Medicament) {
+    m.id = this.medicaments.length + 1; // simplu pt mock
+    this.medicaments = [m, ...this.medicaments];
+  }
+}
